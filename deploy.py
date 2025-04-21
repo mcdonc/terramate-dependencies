ac6b34c1e51e50a8c12dbaf3411df79357ac21c4
Chris McDonough <chrism@plope.com>
2025-04-21 12:34:48 -0400

    refactor

--- deploy.py
+++ deploy.py
@@ -10,17 +10,15 @@ import hcl2 # python-hcl2 package
 class Deployment:
     def __init__(self, args):
         self.args = args
+        self.args.omit = set(self.args.omit)
+        self.args.prune = set(self.args.prune)
+        self.args.stack = set(self.args.stack)
+        self.project_root = self.get_project_root()
 
-        omit = self.args.omit
-        if omit is None:
-            omit = ()
-
-        self.args.omit = set(omit)
-
+    def get_stack_map(self):
         stacks = []
-        self.root = root = self.get_project_root()
 
-        def find_files(filename, search_path=root):
+        def find_files(filename, search_path=self.project_root):
             path = pathlib.Path(search_path)
             return list(path.rglob(filename))
 
@@ -35,23 +33,61 @@ class Deployment:
                     stack["directory"] = os.path.dirname(stackfile)
                     stacks.append(stack)
 
-        self.stacks = stacks
-
         stack_map = {}
         for stack in stacks:
             tags = set(stack.get("tags", []))
-            stack_name = None
             for tag in tags:
                 if tag.startswith("stack."):
-                    stack_name = tag[6:]
                     stack_map[tag] = stack
+
+        return stack_map
+
+    def setup(self):
+        args = self.args
+        stacks = args.stack
+        omits = args.omit
+        prunes = args.prune
+
+        stack_map = self.get_stack_map()
+
+        for stack in stack_map.values():
+            tags = set(stack.get("tags", []))
+            stack_name = None
+
             if stack_name:
                 for tag in tags:
                     if tag.startswith("workspace."):
                         if tag[len("workspace."):] != self.args.workspace:
-                            self.args.omit.add(stack_name)
+                            omits.add(stack_name)
                             print(f"Omitting {stack_name}: foreign ws {tag}")
-        self.stack_map = stack_map
+
+        if not stacks:
+            stacks = set(stack_map.keys())
+        else:
+            stacks = set([ f"stack.{stack}" for stack in stacks ])
+
+        omits = set([ f"stack.{stack}" for stack in omits ])
+        prunes = set([ f"stack.{stack}" for stack in prunes ])
+
+        pruned_edges = self.find_edges(stack_map, stacks, prunes)
+        pruned_deps = self.flatten_edges(pruned_edges)
+
+        final_deps = pruned_deps.difference(omits)
+
+        command = args.command
+
+        return dict(
+            command = command,
+            stacks = stacks,
+            omits = omits,
+            prunes = prunes,
+            pruned_edges = pruned_edges,
+            pruned_deps = pruned_deps,
+            final_deps = final_deps,
+            workspace = args.workspace,
+            unattended = args.unattended,
+            stack_map = stack_map
+        )
 
     def get_project_root(self):
         gitdir = find_directory_upwards(".git")
@@ -67,7 +103,7 @@ class Deployment:
             flattened.add(dst)
         return flattened
 
-    def find_edges(self, target_tags, prunes=None):
+    def find_edges(self, stack_map, target_tags, prunes=None):
         if prunes is None:
             prunes = set()
         else:
@@ -75,16 +111,16 @@ class Deployment:
 
         edges = set()
 
-        for stack_tag, stack in self.stack_map.items():
+        for stack_tag, stack in stack_map.items():
             for before_tag in stack.get("before", []):
                 if before_tag.startswith("tag:stack."):
                     raise ValueError(f"before tags unsupported: {stack_tag}")
 
         def resolve_after_dependencies(stack_tag):
-            if not stack_tag in self.stack_map:
+            if not stack_tag in stack_map:
                 return
 
-            stack = self.stack_map[stack_tag]
+            stack = stack_map[stack_tag]
 
             after_tags = [
                 tag for tag in stack.get("after", []) if
@@ -117,102 +153,88 @@ class Deployment:
 
         return edges
 
-    def run(self):
-        args = self.args
-        stacks = args.stack
-        if not stacks:
-            stacks = set(self.stack_map.keys())
-        else:
-            stacks = set([ f"stack.{stack}" for stack in stacks ])
+    def debug(self, data):
+        stacks = data["stacks"]
+        pruned_edges = data["pruned_edges"]
+        pruned_deps = data["pruned_deps"]
+        omits = data["omits"]
+        final_deps = data["final_deps"]
+        stack_map = data["stack_map"]
+        raw_edges = self.find_edges(stack_map, stacks)
+        raw_deps = self.flatten_edges(raw_edges)
 
-        omits = args.omit
-        if omits is None:
-            omits = set()
-        else:
-            omits = set([ f"stack.{stack}" for stack in omits ])
+        print("Raw Edges")
+        for src, dst in sorted(raw_edges):
+            print(f"  {src} -> {dst}")
+        print("Raw Dependencies")
+        for dep in sorted(raw_deps):
+            print(f"  {dep}")
+        print("Pruned Edges")
+        for src, dst in sorted(pruned_edges):
+            print(f"  {src} -> {dst}")
+        print("Pruned Dependencies")
+        for dep in sorted(pruned_deps):
+            print(f"  {dep}")
+        print("Omits")
+        for dep in sorted(omits):
+            print(f"  {dep}")
+        print("Final Dependencies")
+        for dep in sorted(final_deps):
+            print(f"  {dep}")
 
-        prunes = args.prune
-        if prunes is None:
-            prunes = set()
-        else:
-            prunes = set([ f"stack.{stack}" for stack in prunes ])
+    def deploy(self, data):
+        workspace = data["workspace"]
+        omits = data["omits"]
+        final_deps = data["final_deps"]
+        command = data["command"]
 
-        pruned_edges = self.find_edges(stacks, prunes)
-        pruned_deps = self.flatten_edges(pruned_edges)
+        print(f"using workspace {workspace}")
+        if omits:
+            print(f"omitting stacks {omits}")
+        print(f"{command} {final_deps}")
 
-        final_deps = pruned_deps.difference(omits)
+        tagsopt = ""
+        autoa = ""
+        if data["unattended"]:
+            autoa="-auto-approve"
+        if final_deps:
+            tagsopt = f'--tags={",".join(final_deps)}'
 
-        command = args.command
+        trun = f"terramate run {tagsopt} -X"
+        if command == "destroy":
+            trun = f"{trun} --reverse"
 
-        if command == "debug":
-            raw_edges = self.find_edges(stacks)
-            raw_deps = self.flatten_edges(raw_edges)
+        project_root = self.project_root
 
-            print("Raw Edges")
-            for src, dst in sorted(raw_edges):
-                print(f"  {src} -> {dst}")
-            print("Raw Dependencies")
-            for dep in sorted(raw_deps):
-                print(f"  {dep}")
-            print("Pruned Edges")
-            for src, dst in sorted(pruned_edges):
-                print(f"  {src} -> {dst}")
-            print("Pruned Dependencies")
-            for dep in sorted(pruned_deps):
-                print(f"  {dep}")
-            print("Omits")
-            for dep in sorted(omits):
-                print(f"  {dep}")
-            print("Final Dependencies")
-            for dep in sorted(final_deps):
-                print(f"  {dep}")
+        run(
+            "terramate generate",
+            cwd=project_root
+        )
+        run(
+            "terramate list --run-order",
+            cwd=project_root
+        )
+        run(
+            f"{trun} -- terraform init",
+            cwd=project_root
+        )
+        run(
+            f"{trun} -- terraform workspace select -or-create {workspace}",
+            cwd=project_root
+        )
+        run(
+            "terramate list --run-order",
+            cwd=project_root
+        )
+        run(
+            f"{trun} -- terraform {command} {autoa}",
+            cwd=project_root
+        )
 
-        if command == "graph":
-            self.show_graph(pruned_edges, omits)
-
-        if command in ("apply", "destroy", "plan"):
-            workspace = args.workspace
-            print(f"using workspace {workspace}")
-            if omits:
-                print(f"omitting stacks {omits}")
-            print(f"{command}ing {final_deps}")
-            root = deployment.root
-            tagsopt = ""
-            autoa = ""
-            if args.unattended:
-                autoa="-auto-approve"
-            if final_deps:
-                tagsopt = f'--tags={",".join(final_deps)}'
-            trun = f"terramate run {tagsopt} -X"
-
-            run("terramate generate", cwd=root)
-            run("terramate list --run-order", cwd=root)
-            run(
-                f"{trun} -- terraform init",
-                cwd=root
-            )
-            run(
-                f"{trun} -- terraform workspace select -or-create {workspace}",
-                cwd=root
-            )
-            run("terramate list --run-order", cwd=root)
-            if command == "apply":
-                run(
-                    f"{trun} -- terraform apply {autoa}",
-                    cwd=root
-                )
-            if command == "plan":
-                run(
-                    f"{trun} -- terraform plan",
-                    cwd=root
-                )
-            if command == "destroy":
-                run(
-                    f"{trun} --reverse -- terraform destroy {autoa}",
-                    cwd=root
-                )
-
-    def show_graph(self, edges, omits):
+    def show_graph(self, data):
+        edges = data["pruned_edges"]
+        omits = data["omits"]
+        project_root = self.project_root
         dot = graphviz.Digraph()
         dot.node("__root__", "__root__")
         for s, d in edges:
@@ -229,8 +251,19 @@ class Deployment:
             if s != "__root__":
                 s = s[6:] # "stack."
             dot.edge(s, d[6:])
-        os.chdir(self.get_project_root()) # dont write files everywhere
+        os.chdir(project_root) # dont write files everywhere
         dot.render("infra-graph", format="png", view=True)
+
+    def run(self):
+        data = self.setup()
+        command = data["command"]
+        if command == "debug":
+            self.debug(data)
+        if command == "graph":
+            self.show_graph(data)
+        if command in ("apply", "destroy", "plan"):
+            self.deploy(data)
+
 
 def run(command, **runargs):
     print(f"Running {command}")
@@ -259,30 +292,33 @@ if __name__ == "__main__":
     ap.add_argument(
         "--stack",
         action="append",
-        help="Specify a stack (may be used multiple times)."
-        )
+        help="Specify a stack (may be used multiple times).",
+        default=[]
+    )
     ap.add_argument(
         "--prune",
         action="append",
         help=("Omit this stack and any dependent stacks "
-              "(may be used multiple times).")
-        )
+              "(may be used multiple times)."),
+        default = [],
+    )
     ap.add_argument(
         "--omit",
         action="append",
         help=("Omit this stack, but not any of its dependent stacks "
-              "(may be used multiple times).")
+              "(may be used multiple times)."),
+        default = [],
         )
     ap.add_argument(
         "--unattended",
         action="store_true",
         help="Automatically approve apply or destroy, no questions asked."
-        )
+    )
     ap.add_argument(
         "--workspace",
         help="Specify a deployment workspace",
         default="default"
-        )
+    )
     args = ap.parse_args()
     deployment = Deployment(args)
     deployment.run()
